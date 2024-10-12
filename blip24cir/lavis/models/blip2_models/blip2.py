ff35d49c2262b541e5323530c5adf8774eb8ff5f
BUAADreamer <1428195643@qq.com>
2024-10-12 13:14:33 +0800

    update requirements

--- blip24cir/lavis/models/blip2_models/blip2.py
+++ blip24cir/lavis/models/blip2_models/blip2.py
@@ -45,14 +45,14 @@ class Blip2Base(BaseModel):
 
     @classmethod
     def init_Qformer(cls, num_query_token, vision_width, cross_attention_freq=2):
-        encoder_config = BertConfig.from_pretrained("/root_path/cirdata/models/bert-base-uncased")
+        encoder_config = BertConfig.from_pretrained("bert-base-uncased")
         encoder_config.encoder_width = vision_width
         # insert cross-attention layer every other block
         encoder_config.add_cross_attention = True
         encoder_config.cross_attention_freq = cross_attention_freq
         encoder_config.query_length = num_query_token
         Qformer = BertLMHeadModel.from_pretrained(
-            "/root_path/cirdata/models/bert-base-uncased", config=encoder_config
+            "bert-base-uncased", config=encoder_config
         )
         query_tokens = nn.Parameter(
             torch.zeros(1, num_query_token, encoder_config.hidden_size)
